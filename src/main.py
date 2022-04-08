ca0dadf8208e24c6bf4bd9c8963c98f761702f70
VMishakov <vitaliy.mishakov@gmail.com>
2022-04-08 09:25:25 +0300

    update log

--- src/main.py
+++ src/main.py
@@ -17,8 +17,8 @@ UPDATE_PERIOD = int(os.getenv('TXI_UPDATE_PERIOD', '25'))
 API_TOKEN = os.getenv('TG_TOKEN')
 
 logging.basicConfig(level=logging.INFO, filename=os.getenv('TG_LOG'),
-                    formatter=logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
-                                                "%Y-%m-%d %H:%M:%S"))
+                    format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
